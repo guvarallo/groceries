29dee7b97cd44f858ec106d51b6f701d431299c2
Gustavo Varallo <gustavo.varallo@live.com>
2015-01-07 20:07:41 -0300

    Added users and tasks

---
+++ db/migrate/20150107230343_create_tasks.rb
@@ -0,0 +1,9 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :title, :description
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
