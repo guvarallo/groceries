52088a628ee21b7c8739be7e6ca110a48932a415
Gustavo Varallo <gustavo.varallo@live.com>
2015-01-09 11:21:27 -0300

    Changes for Heroku

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module Groceries
     config.active_record.raise_in_transactional_callbacks = true
 
     #Needed to serve assets on Heroku
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end
