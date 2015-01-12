3e940b0a86a66711b6af41606fbe5d0e3db4eb74
Gustavo Varallo <gustavo.varallo@live.com>
2015-01-12 18:33:10 -0300

    Restricted unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  get  '/sign_up', to: 'users#new'
-  get  '/sign_in', to: 'sessions#new'
-  post '/sign_in', to: 'sessions#create'
+  get  '/sign_up',  to: 'users#new'
+  get  '/sign_in',  to: 'sessions#new'
+  post '/sign_in',  to: 'sessions#create'
   get  '/sign_out', to: 'sessions#destroy'
 
-  resources :users, except: [:new]
-  resources :tasks
+  resources :users, only:   [:create]
+  resources :tasks, except: [:edit, :show]
 end
